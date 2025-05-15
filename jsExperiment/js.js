31ac1d1b00e8f5867a4c57e45ec62c22105cdfca
whuang45 <=>
2025-05-15 11:05:12 -0700

    Made changes to incorperate personal aspects

---
+++ jsExperiment/js.js
@@ -0,0 +1,24 @@
+// let firstname = "Jason"; 
+// let surname = "Huang"
+// console.log(firstname);
+// console.log(surname);
+
+// function jasonfn(name2,name3){
+//     return "Hi" + name2 + "I'm" + name3
+// }
+
+// const person1 = {
+//     name0: "Jason",
+//     age: 3,
+//     greet:jasonfn("Michael",this.name0)
+// }
+const person = {
+    firstname: "1",
+    1: 1123
+};
+
+const PersonTwo = {};
+
+console.log(person[PersonTwo] == null) 
+
+// person1.greet(); 
